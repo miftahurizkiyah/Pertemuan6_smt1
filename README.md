fc45dd7a0f90beb11c1e85f76b3f92391cc46f3c
Miftahu Rizkiyah <mtrizkiyah@gmail.com>
2020-10-31 17:00:39 +0700

    update folder

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Pertemuan6_smt1
-Repository ini dibuat untuk memenuhi tugas bahasa pemrograman pertemuan 6
+Repository ini dibuat untuk memenuhi tugas bahasa pemrograman pertemuan 6 <br> <br>
+Nama : Miftahu Rizkiyah
+
+NIM  : 312010014
+
+Kelas : TI.20.B.1<br><br>
+
+
+
---
+++ tugas_p5.py
@@ -0,0 +1,27 @@
+print("Please enter full name : ")
+fullname=input()
+
+print("please insert your Nickname : ")
+nickname=input()
+
+print("please enter your NPM : ")
+NPM=input()
+
+print("please enter your Born place : ")
+bornplace=input()
+
+print("please insert your age : ")
+age=input()
+
+print("please enter your home address : ")
+address=input()
+
+print("please enter your phone number : ")
+phonenumber=input()
+
+
+print("\nAssalamualaikum Wr, Wb.")
+print(f"\nLet me introduce my self. My name is {fullname}, but you can call me {nickname}. My NPM is {NPM}. I was Born in {bornplace} and i am {age} years old. I am very glad if you want to invite my house in {address}. So, don't forget to call me before with the number {phonenumber}.")
+print("\nThank you.")
+print("\n")
+
